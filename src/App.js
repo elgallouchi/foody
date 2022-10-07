877ea849ee8b103f8fb5639dc2e98dabe0635412
HassanElgallouchi <helgallouchi@gmail.com>
2022-10-07 19:49:34 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Home from "./views/Home";
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import Article from "./views/Article";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SearchResults from "./views/SearchResults";
+
+import { getByCategory, getFromLocalStorage } from "./store/recipeSlice";
+import { useDispatch } from "react-redux";
+import Favorites from "./views/Favorites";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getByCategory());
+    dispatch(getFromLocalStorage());
+  }, []);
   return (
     <Router>
       <Header />
@@ -13,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/article" element={<Article />} />
         <Route path="/results" element={<SearchResults />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
