48187b00df50aaec65a1a0e2ff736d56cc7d6c1b
HassanElgallouchi <helgallouchi@gmail.com>
2022-10-23 17:47:26 +0200

    add area component

---
+++ src/views/Area/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getAllArea } from "../../store/recipeSlice";
+import "./index.css";
+
+export default function Area() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { allArea } = useSelector((state) => state.recipe);
+
+  useEffect(() => {
+    dispatch(getAllArea());
+  }, [dispatch]);
+
+  // redirection to category
+  const btnToCategory = (areaName) => {
+    navigate({
+      pathname: "/results",
+      search: `?q=a&search=${areaName}`,
+    });
+  };
+
+  return (
+    <main id="area">
+      <div className="container">
+        <h2>Area Recipes</h2>
+        <ul>
+          {allArea &&
+            allArea.map((area, index) => (
+              <li key={index} onClick={() => btnToCategory(area.strArea)}>
+                <img
+                  src={`/assets/flags/${
+                    area.strArea === "Unknown" ? "reste" : area.strArea
+                  }.svg`}
+                  alt={area.strArea === "Unknown" ? "reste" : area.strArea}
+                  loading="lazy"
+                />
+                <h3>{area.strArea === "Unknown" ? "Reste" : area.strArea}</h3>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
